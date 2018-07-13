81bfc0bcd80d6322253ec39eeaed3728c07e6b1b
Wilker Lucio <wilkerlucio@gmail.com>
2018-07-13 20:52:17 -0300

    Add dev profile for source paths

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :plugins [[lein-tools-deps "0.4.1"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :lein-tools-deps/config {:config-files [:install :user :project]
-                           :aliases [:dev]})
+                           :aliases [:dev]}
+  :profiles {:dev {:source-paths ["src" "workspaces"]}})
