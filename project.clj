15881761aab622734b57bcbb0843402f7da540bf
Wilker Lucio <wilkerlucio@gmail.com>
2018-08-06 22:50:51 -0300

    Simplified jar dependencies

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
             :url  "https://github.com/nubank/workspaces/blob/master/LICENSE"}
   :plugins [[lein-tools-deps "0.4.1"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
-  :lein-tools-deps/config {:config-files [:install :user :project]
-                           :aliases [:dev]}
+  :lein-tools-deps/config {:config-files [:install :user :project]}
   :jar-exclusions [#"^workspaces/.*" #"^nubank/workspaces/workspaces/.*" #"\.DS_Store" #"^examples/"]
   :deploy-repositories [["releases" :clojars]]
   :aliases {"pre-release"  [["vcs" "assert-committed"]
