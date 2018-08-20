f34b97409d4e21befee6bc7d07f63ca20b5f745e
Wilker Lucio <wilkerlucio@gmail.com>
2018-08-20 20:00:18 -0300

    Bump preview8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nubank/workspaces "1.0.0-preview8-SNAPSHOT"
+(defproject nubank/workspaces "1.0.0-preview8"
   :description "Work environments for development of web apps."
   :url "https://github.com/nubank/workspaces"
   :license {:name "Apache License 2.0"
