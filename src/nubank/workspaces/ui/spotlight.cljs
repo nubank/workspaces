ea5346efefd1946eda253ab4bdebec41b59eebe0
Wilker Lucio <wilkerlucio@gmail.com>
2018-11-25 04:15:28 -0200

    Extract spotlight entry component

--- src/nubank/workspaces/ui/spotlight.cljs
+++ src/nubank/workspaces/ui/spotlight.cljs
@@ -7,13 +7,17 @@
             [fulcro.client.primitives :as fp]
             [fulcro.incubator.ui-state-machines :as fsm]
             [nubank.workspaces.ui.core :as uc]
-            [fulcro-css.css :as css]
             ["../lib/fts_fuzzy_match" :refer [fuzzy_match]]))
 
 (def max-results 50)
 
 (defn value->label [{::keys [id label]}]
   (or label (str id)))
+
+(defn value->key [{::keys [type id] :as opt}]
+  (if (= type ::workspace)
+    (str id)
+    (str (some-> opt ::type name) "-" (value->label opt))))
 
 (defn match-one [filter opt]
   (let [[match? score hl] (fuzzy_match filter (value->label opt))]
@@ -30,13 +34,6 @@
            (take max-results)))
     options))
 
-(defn alias-values [env values]
-  (reduce
-    (fn [out key]
-      (assoc out key (fsm/alias-value env key)))
-    {}
-    values))
-
 (fsm/defstatemachine spotlight-sm
   {::fsm/aliases
    {:search-input    [:spotlight ::filter]
@@ -48,7 +45,6 @@
     {::fsm/handler
      (fn [env]
        (-> env
-           (fsm/store :cache {})
            (fsm/activate :searching)))}
 
     :searching
@@ -56,48 +52,57 @@
      {::fsm/value-changed
       {::fsm/handler
        (fn [env]
-         (let [cache            (fsm/retrieve env :cache)
-               filtered-options (fuzzy-match (alias-values env [:search-input :options]))]
+         (let [filtered-options (fuzzy-match (fsm/aliased-data env))]
            (-> env
                (fsm/set-aliased-value :current-options filtered-options))))}
 
       :exit!
       {::fsm/target-state ::fsm/exit}}}}})
 
-(defn escape-re [input]
-  (let [re (js/RegExp. "([.*+?^=!:${}()|[\\]\\/\\\\])" "g")]
-    (-> input str (.replace re "\\$1"))))
-
-(defn fuzzy-re [input]
-  (-> (reduce (fn [s c] (str s (escape-re c) ".*")) "" input)
-      (js/RegExp "i")))
-
-(defn fuzzy-filter [{::keys [options filter]}]
-  (if (seq filter)
-    (let [fuzzy (fuzzy-re filter)]
-      (->> options
-           (filterv #(re-find fuzzy (value->label %)))
-           (take max-results)
-           (sort-by value->label)))
-    options))
-
-(fm/defmutation filter-results [{::keys [filter]}]
-  (action [{:keys [state ref]}]
-    (let [{::keys [options]} (get-in @state ref)
-          filtered-options (fuzzy-filter {::filter filter ::options options})]
-      (swap! state update-in ref assoc
-        ::filter filter
-        ::filtered-options filtered-options
-        ::value (first filtered-options)))))
-
 (fm/defmutation reset [{::keys [options]}]
   (action [{:keys [state ref]}]
     (let [{::keys [filter]} (get-in @state ref)
           options          (sort-by value->label options)
-          filtered-options (fuzzy-filter {::filter filter ::options options})]
+          filtered-options (fuzzy-match {:search-input filter :options options})]
       (swap! state update-in ref assoc
         ::options options
         ::filtered-options filtered-options))))
+
+(fp/defsc SpotlightEntry
+  [this {::keys [opt value on-change on-select]}]
+  {:css [[:.option
+          {:color         "#1d1d1d"
+           :cursor        "pointer"
+           :font-size     "16px"
+           :padding       "2px 3px"
+           :white-space   "nowrap"
+           :overflow      "hidden"
+           :text-overflow "ellipsis"}
+
+          :b
+          {:color "#000"}]
+
+         [:.option-type
+          {:font-size  "11px"
+           :font-style "italic"}]
+
+         [:.option-selected
+          {:background "#582074"
+           :color      "#fff"}]
+
+         [:.solo-hint
+          {:display "none"}]]}
+  (dom/div :.option {:classes [(if (= opt value) :.option-selected)]
+                     :onClick #(do
+                                 (on-change opt)
+                                 (on-select opt (.-altKey %)))}
+    (dom/div {:dangerouslySetInnerHTML {:__html (or (::match-hl opt) (value->label opt))}})
+    (dom/div :.option-type
+      (some-> opt ::type name)
+      (if (some-> opt ::type (not= ::workspace))
+        (dom/span :.solo-hint " - open solo")))))
+
+(def spotlight-entry (fp/factory SpotlightEntry))
 
 (fp/defsc Spotlight
   [this
@@ -134,38 +139,11 @@
                             :padding     "10px"
                             :width       "100%"}]
 
-                          [:.options
-                           {:font-family uc/font-open-sans
-                            :margin-top  "10px"}]
-
-                          [:.option
-                           {:color         "#1d1d1d"
-                            :cursor        "pointer"
-                            :font-size     "16px"
-                            :padding       "2px 3px"
-                            :white-space   "nowrap"
-                            :overflow      "hidden"
-                            :text-overflow "ellipsis"}
-
-                           :b
-                           {:color "#000"}]
-
-                          [:.option-type
-                           {:font-size  "11px"
-                            :font-style "italic"}]
-
-                          [:.option-selected
-                           {:background "#582074"
-                            :color      "#fff"}]
-
-                          [:.solo-hint
-                           {:display "none"}]
-
                           [:$cljs-workspaces-extended-views
                            [:.option-selected
                             [:.solo-hint
                              {:display "inline"}]]]]
-   :css-include          [cursor/VerticalCursor]
+   :css-include          [cursor/VerticalCursor SpotlightEntry]
    :componentDidMount    (fn []
                            (fsm/begin! this spotlight-sm ::spotlight {:spotlight this})
                            (.select (gobj/get this "input")))
@@ -187,25 +165,12 @@
                               :cursor-factory
                               (fn [opt]
                                 (let [{::keys [value] :as props} (fp/props this)
-                                      {::keys [on-select]
-                                       :or    {on-select identity}}
-                                      (-> props fp/get-computed)
-                                      css (css/get-classnames Spotlight)]
-                                  (dom/div {:classes [(:option css) (if (= opt value) (:option-selected css))]
-                                            :onClick #(do
-                                                        (on-change opt)
-                                                        (on-select opt (.-altKey %)))}
-                                    (dom/div {:dangerouslySetInnerHTML {:__html (or (::match-hl opt) (value->label opt))}})
-                                    (dom/div {:classes [(:option-type css)]}
-                                      (some-> opt ::type name)
-                                      (if (some-> opt ::type (not= ::workspace))
-                                        (dom/span {:classes [(:solo-hint css)]} " - open solo"))))))
-
-                              :cursor-key
-                              (fn [{::keys [type id] :as opt}]
-                                (if (= type ::workspace)
-                                  (str id)
-                                  (str (some-> opt ::type name) "-" (value->label opt))))
+                                      {::keys [on-select] :or {on-select identity}}
+                                      (-> props fp/get-computed)]
+                                  (spotlight-entry {::opt       opt
+                                                    ::value     value
+                                                    ::on-change on-change
+                                                    ::on-select on-select})))
 
                               :cursor-target
                               #(gobj/get this "input")}))}
@@ -226,7 +191,7 @@
                ::cursor/on-change  on-change
                ::cursor/on-select  (fp/get-state this :cursor-select)
                ::cursor/factory    (fp/get-state this :cursor-factory)
-               ::cursor/value->key (fp/get-state this :cursor-key)
+               ::cursor/value->key value->key
                ::dom-events/target (fp/get-state this :cursor-target)})))))))
 
 (def spotlight (fp/factory Spotlight))
