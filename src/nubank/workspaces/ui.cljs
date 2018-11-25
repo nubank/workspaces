4e42450b201418cf0a076be400daf773881dd806
Wilker Lucio <wilkerlucio@gmail.com>
2018-11-25 17:20:04 -0200

    Fix max width on header

--- src/nubank/workspaces/ui.cljs
+++ src/nubank/workspaces/ui.cljs
@@ -926,7 +926,8 @@
                                :font-weight   "bold"
                                :padding       "3px 7px"
                                :box-shadow    "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)"
-                               :margin        "1px 1px 6px"}
+                               :margin        "1px 1px 6px"
+                               :max-width     "100%"}
                      [:button {:color "#fff"}]]
                     [:.row {:display "flex"}]
                     [:.pointer {:cursor "pointer"}]
