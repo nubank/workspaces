bdc3650f5a14174b2d24270c9cda696d080da438
Wilker Lucio <wilkerlucio@gmail.com>
2018-11-25 19:53:27 -0200

    Remove foreign dep definition

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,7 +1,3 @@
-{:foreign-libs
- [{:file     "nubank/workspaces/lib/fts_fuzzy_match.js"
-   :provides ["fts-fuzzy-match"]}]
-
- :npm-deps
+{:npm-deps
  {:react-grid-layout "^0.16.6"
   :highlight.js      "^9.12.0"}}
