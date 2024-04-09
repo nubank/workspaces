eaacadea95ebb2bb02e531d35aa29c7082367298
Isabela Yabuta <isabela.yabuta@nubank.com.br>
2024-04-09 17:52:27 -0300

    Set version to BETA

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nubank/workspaces "1.2.0"
+(defproject nubank/workspaces "1.2.0-BETA"
   :description "Workspaces is a component development environment for ClojureScript."
   :url "https://github.com/nubank/workspaces"
   :license {:name "Apache License 2.0"
